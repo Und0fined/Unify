dc871224ad55a6c6ee319cb48f8685692c9d925c
und0fined <ytcgr09@gmail.com>
2025-03-03 18:50:41 +0800

    Edit

--- learningSamples/chapter_1/server.js
+++ learningSamples/chapter_1/server.js
@@ -1,7 +1,31 @@
 const express = require('express');
 const app = express(); //Creates an Express application
-const PORT = 8383
+const PORT = 8383;
 
+let data = [];
+app.use(express.json());
+
+app.get("/", (req,res)=>{
+    res.send(`
+        <h1>/</h1>
+        <body>${JSON.stringify(data)}</body>`);
+});
+
+
+app.get("/api/data", (req,res)=>{
+    res.send(`
+        <h1>/api/data</h1>
+        <body>${JSON.stringify(data)}</body>`);
+});
+app.post("/api/data",(req,res)=>{
+    const newData = req.body;
+    data.push(newData);
+    res.sendStatus(201);
+});
+app.delete("/api/data/endpoint",(req,res)=>{
+    data.pop();
+    res.sendStatus(202);
+});
 app.listen(PORT, ()=>{
-    console.log(`Server Started | Port: ${PORT}`)
+    console.log(`Server Started | Port: ${PORT}`);
 });
